0118bc81aa9dc5ba2fef6bd192b212bdd07d4870
Alexander Fyrdahl <alexander.fyrdahl@ki.se>
2019-08-13 21:57:15 +0200

    Do not run octave in background

--- sync-code-and-start-fire-matlab-server.sh
+++ sync-code-and-start-fire-matlab-server.sh
@@ -4,7 +4,7 @@ cp -R -f /tmp/share/code/* "/opt/code/fire-matlab-ismrmrd-server/"
 
 if [ $# -eq 1 ]; then
     LOG_FILE=${1}
-    octave-cli --eval "cd /opt/code/fire-matlab-ismrmrd-server/; fire_matlab_ismrmrd_server(9002,'${LOG_FILE}')" &
+    octave-cli --eval "cd /opt/code/fire-matlab-ismrmrd-server/; fire_matlab_ismrmrd_server(9002,'${LOG_FILE}')"
 else
-    octave-cli --eval "cd /opt/code/fire-matlab-ismrmrd-server/; fire_matlab_ismrmrd_server(9002)" &
+    octave-cli --eval "cd /opt/code/fire-matlab-ismrmrd-server/; fire_matlab_ismrmrd_server(9002)"
 fi
